ad36c70b23a78019c9b0c1e3f1f95706b6ed0e84
muser <muser.talk@gmail.com>
2017-12-23 10:09:34 +0200

    Make a dir ./data if there isn't because i ignore on .gitigonore and when run the first time the app this folder it doesn't exist so it must be created.

--- pomodoro.py
+++ pomodoro.py
@@ -85,25 +85,39 @@ def notification(concern, time_spent, starting_time,
 def write_stats(starting_date, starting_time, concern, time_spent, finish_time):
     """ Write some stats on a csv file. """
 
-    path = pathlib.Path('data/time-management.csv')
+    path_file = pathlib.Path('data/time-management.csv')
+    path_dir = pathlib.Path('./data')
 
-    if path.is_file():
-        with open('data/time-management.csv', "a") as f:
-            f.write("\n{0}, {1}, {2}, {3}, {4}".format(starting_date,
-                                                       starting_time,
-                                                       concern,
-                                                       time_spent,
-                                                       finish_time))
+    if path_dir.is_dir():
+        if path_file.is_file():
+            with open('data/time-management.csv', "a") as f:
+                f.write("\n{0}, {1}, {2}, {3}, {4}".format(starting_date,
+                                                           starting_time,
+                                                           concern,
+                                                           time_spent,
+                                                           finish_time))
+        else:
+            with open('data/time-management.csv', "w") as f:
+                f.write("Starting Date, Starting Time, Concern, Time Spent,"
+                        " Finish Time")
+                f.write("\n")
+                f.write("{0}, {1}, {2}, {3}, {4}".format(starting_date,
+                                                         starting_time,
+                                                         concern,
+                                                         time_spent,
+                                                         finish_time))
     else:
+        path_dir.mkdir()
+
         with open('data/time-management.csv', "w") as f:
-            f.write("Starting Date, Starting Time, Concern, Time Spent,"
-                    " Finish Time")
-            f.write("\n")
-            f.write("{0}, {1}, {2}, {3}, {4}".format(starting_date,
-                                                     starting_time,
-                                                     concern,
-                                                     time_spent,
-                                                     finish_time))
+                        f.write("Starting Date, Starting Time, Concern,"
+                                " Time Spent, Finish Time")
+                        f.write("\n")
+                        f.write("{0}, {1}, {2}, {3}, {4}".format(starting_date,
+                                                                 starting_time,
+                                                                 concern,
+                                                                 time_spent,
+                                                                 finish_time))
 
 
 def consume_time(ring=60):
